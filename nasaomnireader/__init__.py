0fa82cdc1a97ce680b2e1fb5a2c4b8c9ad41feb1
Liam Kilcommons <lkilcommons@users.noreply.github.com>
2021-11-12 15:27:27 -0700

    Remove option of custom config python file

--- nasaomnireader/__init__.py
+++ nasaomnireader/__init__.py
@@ -1,8 +1,3 @@
 # (C) 2020 University of Colorado AES-CCAR-SEDA (Space Environment Data Analysis) Group
 # Written by Liam M. Kilcommons
-import traceback
-try:
-    from nasaomnireader.omnireader_config import config
-except:
-    print(traceback.format_exc())
-    from nasaomnireader.default_config import config
+from nasaomnireader.default_config import config
